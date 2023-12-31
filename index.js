01a57c1b47fc9858eba78941cfba764f3d2918bb
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2023-12-31 16:43:26 +0600

    resoled all issue

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Hello Github')
+
+console.log('I Resolved The Problem of Git-github intrigation');
+
+console.log('resolve git another problem.');
+
+const welcome = 'Hello JavaScript';
+console.log(welcome);
